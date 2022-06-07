27aab15003cdc806559cfb63b1d94d4fcfe0e848
Dan Lee <danlee0630@gmail.com>
2022-06-07 17:30:32 +0100

    Updates

---
+++ precache-manifest.48c4c34894f462af49dac3432b7e37cc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d49317f787ce68542c89da520a2da57",
+    "url": "/socialSite/index.html"
+  },
+  {
+    "revision": "11b3313a78e9f80035bb",
+    "url": "/socialSite/static/css/2.7c5c0350.chunk.css"
+  },
+  {
+    "revision": "cea51d1191f097bceace",
+    "url": "/socialSite/static/css/main.8785ace8.chunk.css"
+  },
+  {
+    "revision": "11b3313a78e9f80035bb",
+    "url": "/socialSite/static/js/2.9ffdac5e.chunk.js"
+  },
+  {
+    "revision": "2cb479a22703689952d91fd12ba22fbf",
+    "url": "/socialSite/static/js/2.9ffdac5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea51d1191f097bceace",
+    "url": "/socialSite/static/js/main.0eb4dcbb.chunk.js"
+  },
+  {
+    "revision": "c20ef2aa08899df34971",
+    "url": "/socialSite/static/js/runtime-main.c16e3f36.js"
+  },
+  {
+    "revision": "630f7fda85f7b35cb42cbe1be64ec8f0",
+    "url": "/socialSite/static/media/FaceTube.630f7fda.png"
+  },
+  {
+    "revision": "931a98bc05e592b9385c9e93315f65b7",
+    "url": "/socialSite/static/media/dislike.931a98bc.png"
+  },
+  {
+    "revision": "d1fd0755f74a9609c0333dd29daebdb2",
+    "url": "/socialSite/static/media/like.d1fd0755.png"
+  }
+]);
